dd83f92e7f011f5a3afc8a94124a384b1a3952aa
One Dionys <161098412+onedionys@users.noreply.github.com>
2024-03-18 06:14:52 +0700

    chore: initialize function project

---
+++ src/corsMiddleware.js
@@ -0,0 +1,11 @@
+// corsMiddleware.js
+
+// Middleware function to handle CORS headers
+const corsMiddleware = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+};
+
+module.exports = corsMiddleware;
